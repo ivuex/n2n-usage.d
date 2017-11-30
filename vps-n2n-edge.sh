d27c957633f902cc023ffb4354af610b9f0a7c70
ivuex <33278186+ivuex@users.noreply.github.com>
2017-11-30 12:25:40 +0800

    Update vps-n2n-edge.sh

--- vps-n2n-edge.sh
+++ vps-n2n-edge.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
- sudo edge -d edge0 -a 10.0.0.11 -c mynetwork -u 1000 -g 1000 -k password -l 118.193.152.107:5000 -M 1200;
+ sudo edge -d edge0 -a 10.0.0.11 -c mynetwork -u 1000 -g 1000 -k password -l 118.193.152.107:5000 -M 1200
